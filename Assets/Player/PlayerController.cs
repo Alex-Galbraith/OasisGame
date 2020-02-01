98509cd7d5bb9c267ee56958f60b7248ecfc0dbe
Josiah Hunt <josiahofhunt@gmail.com>
2020-02-01 13:56:11 +1300

    Added a prototype scene with a player controller and item pickups.

---
+++ Assets/Player/PlayerController.cs
@@ -0,0 +1,62 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    [HideInInspector]
+    public Rigidbody rb;
+
+    private float speed = 4f;
+    private Vector3 direction;
+    private float lookSpeed = 1f;
+    private Quaternion lookRotation;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        var forward = Input.GetAxisRaw("Vertical");
+        var horizontal = Input.GetAxisRaw("Horizontal");
+
+        if (forward != 0 || horizontal != 0) DoMove(forward, horizontal);
+        UpdateFacing();
+        if (forward == -1f)
+        {
+            rb.drag = 4f;
+        }
+        else
+        {
+            rb.drag = 0.5f;
+        }
+
+    }
+
+    void DoMove (float f, float h)
+    {
+        Vector3 forwardVector = Vector3.zero;
+        Vector3 horizontalVector = Vector3.zero;
+        if (f == 1) forwardVector = transform.forward;
+        if (h == -1) horizontalVector = -transform.right;
+        if (h == 1) horizontalVector = transform.right;
+
+        direction = (forwardVector + horizontalVector);
+        rb.AddForce(forwardVector * speed);
+
+    }
+
+    void UpdateFacing()
+    {
+        if (direction != Vector3.zero)
+        {
+            lookRotation = Quaternion.LookRotation(direction);
+            transform.rotation = Quaternion.Slerp(transform.rotation, lookRotation, Time.deltaTime * lookSpeed);
+        }
+    }
+}
---
+++ Assets/Upgrades/All/Pickup.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Pickup : MonoBehaviour
+{
+    public AudioSource audioPlayer;
+    public AudioClip pickupSound;
+
+    private void OnTriggerEnter(Collider collision)
+    {
+        audioPlayer.PlayOneShot(pickupSound);
+        if (collision.CompareTag("Player"))
+        {
+            Destroy(this.gameObject);
+        }
+        
+    }
+}
